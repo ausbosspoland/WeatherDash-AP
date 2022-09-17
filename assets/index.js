cf80124cdea5032bcc4f6fbb6c8cee2c67f4e588
ausbosspoland <austinjpoland311@gmail.com>
2022-09-17 15:43:03 -0500

    getting values for lat and lon for api url call

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,6 @@
 var citySearchForm = document.querySelector('.citysearchform');
 var citySearchInput = document.querySelector('#searchcityname');
+var API_KEY = '958bf1982262f64b477214a11d1708fc';
 
 citySearchForm.addEventListener("submit", )
 
@@ -12,5 +13,20 @@ function cityUSerInput(event) {
 function getLatLon(cityName) {
 
     console.log(cityName);
+
+    var latLonUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${API_KEY}`
+
+    console.log("here", latLonUrl);
+
+    fetch(latLonUrl)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data)
+            var lat = data[0].lat
+            var lon = data[0].lon
+            console.log(lat, lon)
+        })
     
 }
